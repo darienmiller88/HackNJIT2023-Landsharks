9c485fca004cc766b3ec05ef218d8b2cd74f1fdd
habeebatje <habeebo02@hotmail.com>
2023-11-05 11:58:24 -0500

    final push

---
+++ GCSauth.py
@@ -0,0 +1,5 @@
+from google.oauth2 import service_account
+
+credentials = service_account.Credentials.from_service_account_file('C:\Users\habee\AppData\Local\Temp\MicrosoftEdgeDownloads\cef38edf-64d1-439e-8968-555714ec6311\hacknjit2023-404120-c017e632b74a.json')
+
+
